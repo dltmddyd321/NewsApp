ce9a46b2d4c56fc98cd412cd639cd27f5aed8d67
dltmddyd321 <dltmddyd321@naver.com>
2021-09-07 20:36:13 +0900

    2021_09_07
    코로나 감염 현황 조회 API 연동 테스트
    -> 성공!
    
    + 레이아웃 요소에 연결 요망
    
    + 뉴스 앱에 바이러스 정보 버튼 생성하여 Intent로 넘어가기
    
    + 실시간 날짜에 해당하는 데이터 요망

---
+++ Covid_tracker/app/src/main/java/com/example/covid_tracker/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.covid_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    String key = "SRfaVCVtmpXmxrec%2FKI%2B2lVTRevXOkSEwl7COrPvsLlGcFxZp1rkpft3QlkOFRkGJK%2FOG39WItiH9Bu32AXbHA%3D%3D";
+    String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text= (TextView)findViewById(R.id.text);
+    }
+
+    public void mOnClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data = getXmlData();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(data);
+                            }
+                        });
+                    }
+                }).start();
+                break;
+        }
+    }
+
+
+    String getXmlData() {
+        StringBuffer buffer = new StringBuffer();
+
+        String queryUrl = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=" + key
+                + "&pageNo=1&numOfRows=10&startCreateDt=20200310&endCreateDt=20200315";
+
+        try {
+            URL url = new URL(queryUrl);
+            InputStream inputStream = url.openStream();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new InputStreamReader(inputStream, "UTF-8"));
+
+            String tag;
+
+            xmlPullParser.next();
+            int eventType = xmlPullParser.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag = xmlPullParser.getName();
+
+                        if (tag.equals("item"));
+                        else if (tag.equals("accDefRate")) {
+                            buffer.append("accDefRate : ");
+                            xmlPullParser.next();
+                            buffer.append(xmlPullParser.getText());
+                            buffer.append("\n");
+                        }
+
+                        else if (tag.equals("accExamCnt")) {
+                            buffer.append("accExamCnt : ");
+                            xmlPullParser.next();
+                            buffer.append(xmlPullParser.getText());
+                            buffer.append("\n");
+                        }
+
+                        else if (tag.equals("accExamCompCnt")) {
+                            buffer.append("accExamCompCnt : ");
+                            xmlPullParser.next();
+                            buffer.append(xmlPullParser.getText());
+                            buffer.append("\n");
+                        }
+
+                        else if (tag.equals("careCnt")) {
+                            buffer.append("careCnt : ");
+                            xmlPullParser.next();
+                            buffer.append(xmlPullParser.getText());
+                            buffer.append("\n");
+                        }
+
+                        else if (tag.equals("clearCnt")) {
+                            buffer.append("clearCnt : ");
+                            xmlPullParser.next();
+                            buffer.append(xmlPullParser.getText());
+                            buffer.append("\n");
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+
+                        tag = xmlPullParser.getName();
+                        if(tag.equals("careCnt"))buffer.append("\n");
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        buffer.append("파싱 종료\n");
+        return buffer.toString();
+    }
+}
