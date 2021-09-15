a4c42abd54be052cdff0572c4b4fb47591ed5cd8
dltmddyd321 <dltmddyd321@naver.com>
2021-09-15 16:39:23 +0900

    2021_09_15
    2. 주석 처리

--- News_Kor/app/src/main/java/com/example/news_kor/CovidTracker.java
+++ News_Kor/app/src/main/java/com/example/news_kor/CovidTracker.java
@@ -23,6 +23,7 @@ public class CovidTracker extends AppCompatActivity {
     String key = "SRfaVCVtmpXmxrec%2FKI%2B2lVTRevXOkSEwl7COrPvsLlGcFxZp1rkpft3QlkOFRkGJK%2FOG39WItiH9Bu32AXbHA%3D%3D";
     String decideCnt, clearCnt, careCnt, stateDt, deathCnt;
 
+    //현재 날짜를 API 요청을 위해 반환
     @SuppressLint("SimpleDateFormat")
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
 
@@ -45,6 +46,7 @@ public class CovidTracker extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
+                        //각각의 XML 데이터를 getCoronaData를 통해 파싱
                         decideCnt = getCoronaData("decideCnt","확진자 수 : ","명");
                         stateDt = getCoronaData("stateDt","현재 일자 : ","명");
                         clearCnt = getCoronaData("clearCnt","완치 수 : ","명");
@@ -54,6 +56,7 @@ public class CovidTracker extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
+                                //TextView를 가져온 API 데이터에 매칭
                                 state.setText(stateDt);
                                 decide.setText(decideCnt);
                                 clear.setText(clearCnt);
@@ -68,17 +71,23 @@ public class CovidTracker extends AppCompatActivity {
     }
 
     String getCoronaData(String xml, String value, String unit) {
+        //현재 날짜 받기
         Date date = new Date();
         String today = simpleDateFormat.format(date);
+
+        //문자열을 받아오고 붙일 Buffer 생성
         StringBuffer buffer = new StringBuffer();
 
+        //데이터를 요청하는 쿼리 주소 선언 (현재 날짜에 따라 데이터 파싱)
         String queryUrl = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=" + key
                 + "&pageNo=1&numOfRows=10&startCreateDt=" + today;
 
         try {
+            //쿼리 주소로 값을 받아오기 위해 URL, Stream 선언
             URL url = new URL(queryUrl);
             InputStream inputStream = url.openStream();
 
+            //Xml 데이터를 Stream을 통해 받아오기
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser xmlPullParser = factory.newPullParser();
             xmlPullParser.setInput(new InputStreamReader(inputStream, "UTF-8"));
@@ -89,14 +98,17 @@ public class CovidTracker extends AppCompatActivity {
             int eventType = xmlPullParser.getEventType();
 
             while (eventType != XmlPullParser.END_DOCUMENT) {
+                //자료의 끝이 아닌 이상 함수 계속 실행
                 switch (eventType) {
                     case XmlPullParser.START_DOCUMENT:
+                        //Xml 자료 내용이 존재할 때 파싱 시작
                         buffer.append("파싱 시작\n\n");
                         break;
 
                     case XmlPullParser.START_TAG:
                         tag = xmlPullParser.getName();
 
+                        //Xml 자료의 Tag 값을 지정하여 내부의 데이터를 파싱
                         if (tag.equals("item"));
                         else if (tag.equals(xml)) {
                             buffer.append(value);
@@ -119,7 +131,7 @@ public class CovidTracker extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //buffer.append("파싱 종료\n");
+        //파싱 종료 후 파싱한 데이터 값을 문자열로 반환
         return buffer.toString();
     }
 
